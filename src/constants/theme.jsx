fb25dc74c9bfafb5508506ba3f3512e239bff216
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-11-09 21:16:13 +0200

    add theme provider

---
+++ src/constants/theme.jsx
@@ -0,0 +1,13 @@
+export const theme = Object.freeze({
+  colors: {
+    primaryFont: '#111111',
+    mainBackground: '#c0c0c0',
+  },
+  border: {
+    onePxSolid: '1px solid',
+  },
+  font: {
+    small: '0.8em',
+    medium: '1.3em',
+  },
+});
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from 'constants';
 import { App } from 'components/App';
-import './index.css';
+import './reset.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
